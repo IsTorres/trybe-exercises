6a18b8f2dfd50f8709ac71292824e8ed1c61cfc2
isTorres <ismaeltorres612@gmail.com>
2021-02-24 22:22:17 -0300

    exerc 10

--- fundamentos-web/bloco_4/dia_1/exercises/peloMenosUmImpar.js
+++ fundamentos-web/bloco_4/dia_1/exercises/peloMenosUmImpar.js
@@ -2,6 +2,6 @@ let num1 = 123;
 let num2 = 456;
 let num3 = 789;
 
-if((num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0)){
+if((num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0)){
   console.log(true);
 }
