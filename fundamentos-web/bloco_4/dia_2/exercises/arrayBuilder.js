c5a392ca85fb30d98a774f6edb83f3f3e44130b3
isTorres <ismaeltorres612@gmail.com>
2021-02-25 19:16:13 -0300

    exerc 9 refactory 8

--- fundamentos-web/bloco_4/dia_2/exercises/arrayBuilder.js
+++ fundamentos-web/bloco_4/dia_2/exercises/arrayBuilder.js
@@ -1,7 +1,7 @@
-let array = [];
+let numbers = [];
 
-for(let index = 1; index < 26; index += 1){
-  array.push(index) ;
+for(let index = 1; index <= 25; index += 1){
+  numbers.push(index) ;
 }
 
-console.log(array);
+console.log(numbers);
