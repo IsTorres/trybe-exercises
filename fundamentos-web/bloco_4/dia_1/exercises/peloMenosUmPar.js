41ca56d39604e0a8fabb0717ed948a8a781a3f06
isTorres <ismaeltorres612@gmail.com>
2021-02-24 21:37:15 -0300

    exerc 7 e 8

---
+++ fundamentos-web/bloco_4/dia_1/exercises/peloMenosUmPar.js
@@ -0,0 +1,13 @@
+let num1 = 123;
+let num2 = 456;
+let num3 = 789;
+
+if((num1 % 2) == 0){
+  console.log(true);
+} else if((num2 % 2) == 0){
+  console.log(true);
+} else if((num3 % 2) == 0 ){
+  console.log(true);
+} else {
+  console.log(false);
+}
