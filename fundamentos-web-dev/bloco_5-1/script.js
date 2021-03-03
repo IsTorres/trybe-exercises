735b7392924598722f0b35f8f9465eb18a1aa158
isTorres <ismaeltorres612@gmail.com>
2021-03-03 19:01:26 -0300

    exerc 1, 2

--- fundamentos-web-dev/bloco_5-1/script.js
+++ fundamentos-web-dev/bloco_5-1/script.js
@@ -1,12 +1,26 @@
 /*
-        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-        - document.getElementById()
-        - document.getElementsByClassName()
-        - document.getElementsByTagName()
- Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
- Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
- Crie uma função que mude a cor do quadrado vermelho para branco.
- Crie uma função que corrija o texto da tag <h1>.
- Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
- Crie uma função que exiba o conteúdo de todas as tags <p> no console.
-        */
+Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+       - document.getElementById()
+       - document.getElementsByClassName()
+       - document.getElementsByTagName()
+Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+Crie uma função que mude a cor do quadrado vermelho para branco.
+Crie uma função que corrija o texto da tag <h1>.
+Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+*/
+
+// exe1:
+function toTwoYears() {
+  let paragraph = document.getElementsByTagName('p')[1];
+  paragraph.innerHTML = 'Me vejo evoluindo sempre! :)'
+}
+toTwoYears();
+
+// exe2:
+function trybeGreen() {
+  let square = document.getElementsByClassName('main-content')[0];
+  square.style.background = 'rgb(76,164,109)';
+}
+trybeGreen();
