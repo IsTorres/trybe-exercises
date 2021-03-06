fc2afa479d04c3c0cecb1349d1393b3d41a8fd1a
isTorres <ismaeltorres612@gmail.com>
2021-03-05 23:25:22 -0300

    exerc 1 - create days

--- fundamentos-WebDev/bloco_5-3/script.js
+++ fundamentos-WebDev/bloco_5-3/script.js
@@ -10,7 +10,19 @@ function createDaysOfTheWeek() {
     weekDaysList.appendChild(dayListItem);
   };
 };
-
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+function createDaysOfMonth () {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+  const monthDays = document.querySelector('#days');
+
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    const days = dezDaysList[i];
+    const monthDayListItem = document.createElement('li');
+    monthDayListItem.innerHTML = days;
+
+    monthDays.appendChild(monthDayListItem);
+  }
+}
+createDaysOfMonth();
