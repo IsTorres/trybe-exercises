68c8baa94e7301272623aea06201c81b86990f1d
isTorres <ismaeltorres612@gmail.com>
2021-03-24 21:06:14 -0300

    part 2 (exe 1,2,3)

---
+++ fundamentosWebDev/part2.js
@@ -0,0 +1,34 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+/*
+  1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+*/
+const addTurno = (obj, key, value) => obj[key] = value;
+addTurno(lesson2, 'turno', 'manhã');
+
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+console.log(lesson2);
+
+const objKey = (obj) => Object.keys(obj);
+console.log(objKey(lesson3));
+
+// 3 - Crie uma função para mostrar o tamanho de um objeto.
+const sizeObj = (obj) => Object.keys(obj).length;
+console.log(sizeObj(lesson1));
