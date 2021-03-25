7a652a6e75b49da8bba0df3f524614af6feb2de0
isTorres <ismaeltorres612@gmail.com>
2021-03-24 21:27:55 -0300

    part 2 (exe 4,5)

--- fundamentosWebDev/part2.js
+++ fundamentosWebDev/part2.js
@@ -1,3 +1,24 @@
+
+/*
+  1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+*/
+const addTurno = (obj, key, value) => obj[key] = value;
+addTurno(lesson2, 'turno', 'manhã');
+
+// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+console.log(lesson2);
+
+const objKey = (obj) => Object.keys(obj);
+console.log(objKey(lesson3));
+
+// 3 - Crie uma função para mostrar o tamanho de um objeto.
+const sizeObj = (obj) => Object.keys(obj).length;
+console.log(sizeObj(lesson1));
+
+// 4 - Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro
+const listValues = (obj) => Object.values(obj);
+console.log(listValues(lesson1));
+
 const lesson1 = {
   materia: 'Matemática',
   numeroEstudantes: 20,
@@ -17,18 +38,7 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite',
 };
-/*
-  1 - Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
-*/
-const addTurno = (obj, key, value) => obj[key] = value;
-addTurno(lesson2, 'turno', 'manhã');
 
-// 2 - Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
-console.log(lesson2);
-
-const objKey = (obj) => Object.keys(obj);
-console.log(objKey(lesson3));
-
-// 3 - Crie uma função para mostrar o tamanho de um objeto.
-const sizeObj = (obj) => Object.keys(obj).length;
-console.log(sizeObj(lesson1));
+// 5 - Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . 
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons);
