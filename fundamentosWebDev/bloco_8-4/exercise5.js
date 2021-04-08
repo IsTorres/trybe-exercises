62720e5e49c829da01cfa1fa0d69734b26154c1b
isTorres <ismaeltorres612@gmail.com>
2021-04-08 10:25:22 -0300

    exercise 5 refactored

--- fundamentosWebDev/bloco_8-4/exercise5.js
+++ fundamentosWebDev/bloco_8-4/exercise5.js
@@ -10,7 +10,15 @@ const names = [
 // Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
 function containsA() {
   // escreva seu código aqui
-  return names.reduce((acNames, cName) => )
+  return names.reduce((acNames, cName) => acNames + cName.split('').reduce((acc, letra) => 
+    letra === 'A' || letra === 'a' ? acc + 1 : acc, 0), 0);
 }
 
+
 assert.deepStrictEqual(containsA(), 20);
+
+// function dale() {
+//   const x = 'Adervandes';
+//   return x.split('').reduce((acc, letra) => letra === 'A' || letra === 'a' ? acc + 1 : acc, 0)
+// }
+// console.log(dale());
