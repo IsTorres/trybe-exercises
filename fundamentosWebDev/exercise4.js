9283a41211c90df3931e3b0b3018737c221c8124
isTorres <ismaeltorres612@gmail.com>
2021-04-12 19:05:53 -0300

    exercise 5(in the same file of 4)

--- fundamentosWebDev/exercise4.js
+++ fundamentosWebDev/exercise4.js
@@ -8,9 +8,22 @@ const getMarsTemperature = () => {
 };
 
 // crie a função sendMarsTemperature abaixo
-function sendMarsTemperature(getMarsTemp) {
+function sendMarsTemperatureA(getMarsTemp) {
   setTimeout(() => console.log(`Mars temperature is: ${getMarsTemp} degree Celcius`), messageDelay());
 }
 
+sendMarsTemperatureA(getMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
 
-sendMarsTemperature(getMarsTemperature()); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+// Agora que você fez a função que envia a temperatura de Marte, suponha que você consiga enviar para o robô Curiosity o que você deseja fazer, uma vez obtida com sucesso a temperatura em Marte. Logo, adicione na função sendMarsTemperature um callback que contenha as ações a serem tomadas em cima da temperatura.
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+const temperatureInFahrenheit = (temperature) => console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+const greet = (temperature) => console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+// definição da função sendMarsTemperature...
+function sendMarsTemperature(getMarsTemp) {
+  setTimeout(() => getMarsTemp(toFahrenheit(getMarsTemperature())), messageDelay()); // Encapsula 3 funções
+}
+
+sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
+sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
