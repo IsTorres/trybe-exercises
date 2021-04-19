e4b320236a3fe393547b7f78e7e1ab536a0f168a
isTorres <ismaeltorres612@gmail.com>
2021-04-19 18:49:51 -0300

    functoi sum

--- fundamentosWebDev/bloco_10/sum.js
+++ fundamentosWebDev/bloco_10/sum.js
@@ -1,16 +1,11 @@
 // const assert = require('assert');
 
-// function sum(a, b) {
-//   if (typeof a !== 'number' || typeof b !== 'number') {
-//     throw new Error('parameters must be numbers');
-//   }
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
 
-//   return a + b;
-// }
-
-// sum.js
-const sum = (a, b) => a + b;
+  return a + b;
+}
 
 module.exports = sum;
-
-// implemente seus testes aqui
--- fundamentosWebDev/bloco_10/sum.test.js
+++ fundamentosWebDev/bloco_10/sum.test.js
@@ -1,5 +1,19 @@
 const sum = require('./sum');
 
-test('sums two values', () => {
-  expect(sum(2, 3)).toEqual(5);
-});
+describe('sum', () => {
+  test('4 plus 5 is 9', () => {
+    expect(sum(4, 5)).toEqual(9);
+  });
+
+  test('0 plus 0 is 0', () => {
+    expect(sum(0, 0)).toEqual(0);
+  });
+
+  test('message of error is "parameters must be numbers"', () => {
+    expect(() => sum(4,'5')).toThrow('parameters must be numbers');
+  })
+
+  test('Throw a error if any element arent a number', () => {
+    expect(() => sum('a','b')).toThrow();
+  });
+})
