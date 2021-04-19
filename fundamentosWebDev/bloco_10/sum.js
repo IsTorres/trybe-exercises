b3f0e98fa630f9972a87220d6a47636c946b46a7
isTorres <ismaeltorres612@gmail.com>
2021-04-19 16:05:27 -0300

    exercise bloco 10 - Tests Jest

---
+++ fundamentosWebDev/bloco_10/sum.js
@@ -0,0 +1,16 @@
+// const assert = require('assert');
+
+// function sum(a, b) {
+//   if (typeof a !== 'number' || typeof b !== 'number') {
+//     throw new Error('parameters must be numbers');
+//   }
+
+//   return a + b;
+// }
+
+// sum.js
+const sum = (a, b) => a + b;
+
+module.exports = sum;
+
+// implemente seus testes aqui
