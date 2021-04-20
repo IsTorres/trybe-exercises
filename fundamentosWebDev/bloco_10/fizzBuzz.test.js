cc3b3aa8f8559c3987f1f0bb1846b94994a93f54
isTorres <ismaeltorres612@gmail.com>
2021-04-19 23:07:53 -0300

    exercises fizzbuzz whit jest

--- fundamentosWebDev/bloco_10/fizzBuzz.test.js
+++ fundamentosWebDev/bloco_10/fizzBuzz.test.js
@@ -3,5 +3,21 @@ const fizzbuzz = require('./fizzBuzz');
 describe('FizzBuzz', () => {
   test('test if the returt is fizzbuzz', () => {
     expect(fizzbuzz(15)).toEqual('fizzbuzz');
-  })
+  });
+
+  test('test fizz, divisible by 3', () => {
+    expect(fizzbuzz(9)).toEqual('fizz');
+  });
+
+  test('test buzz, divisible by 5', () => {
+    expect(fizzbuzz(10)).toEqual('buzz');
+  });
+
+  test('test a number not divisible by 3 or 5', () => {
+    expect(fizzbuzz(13)).toEqual(13);
+  });
+
+  test('test not a number', () => {
+    expect(fizzbuzz('a')).toEqual(false);
+  });
 })
