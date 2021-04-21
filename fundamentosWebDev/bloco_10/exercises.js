a696fbcdebb824e07ca75e37024cb323eececa7b
isTorres <ismaeltorres612@gmail.com>
2021-04-20 22:06:04 -0300

    exercise 2

--- fundamentosWebDev/bloco_10/exercises.js
+++ fundamentosWebDev/bloco_10/exercises.js
@@ -1,8 +1,29 @@
+// Exe.: 1
 const uppercase = (str, callback) => {
   callback(str.toUpperCase());
 };
-console.log(uppercase('test', (str) => str));
+
+// Exe.: 2 e 3
+const users = {
+  4: { name: 'Mark' },
+  5: { name: 'Paul' }
+  };
+  
+  const findUserById = (id) => {
+    return new Promise((resolve, reject) => {
+        if (users[id]) {
+          return resolve(users[id]);
+        };
+  
+        return reject({ error: 'User with ' + id + ' not found.' });
+    });
+  };
+  
+  const getUserName = (userID) => {
+    return findUserById(userID).then(user => user.name);
+  };
+
+  // console.log(getUserName(1));
 //
 
-module.exports = 
-  uppercase;
+module.exports = { uppercase, getUserName };
--- fundamentosWebDev/bloco_10/exercises.test.js
+++ fundamentosWebDev/bloco_10/exercises.test.js
@@ -1,8 +1,29 @@
-const uppercase = require('./exercises');
+const {
+  uppercase,
+  getUserName,
+ } = require('./exercises');
+
 
 describe('Tests for uppercase function', () => {
-  it('test word test', () => { // (done)
+  it('test with uppercase return TEST', () => { // (done)
     uppercase('test', (str) => expect(str).toBe('TEST'));
     // done();
+  });
+});
+
+describe('Tests for getUseName function', () => {
+  it('find the user with ID 4', async () => {
+    await expect(getUserName(4)).resolves.toEqual('Mark');
+  });
+
+  it('find the user with ID 5', async () => {
+    await expect(getUserName(5)).resolves.toEqual('Paul');
+  });
+
+  it('not find the user', async () => {
+    // const id = 1;
+    // await expect(getUserName(id)).rejects.toMatch({ error: `User with ${id} not found.` });
+    expect.assertions(1);
+    await expect(getUserName(2)).rejects.toEqual({ error: 'User with 2 not found.'})
   })
 })
