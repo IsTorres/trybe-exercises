ff1b41f9e30fb01faa7504afc2dd69b22bae1a05
isTorres <ismaeltorres612@gmail.com>
2021-09-02 21:09:56 -0300

    cria modulos create e isValid

--- back-end/Bloco_26/model-example/models/Author.js
+++ back-end/Bloco_26/model-example/models/Author.js
@@ -1,7 +1,7 @@
 const connection = require('./connection');
 
 const getNewAuthor = ({id, firstName, middleName, lastName}) => {
-  const fullName = [firstName, middleName, lastName]. filter((name) => name)
+  const fullName = [firstName, middleName, lastName].filter((name) => name)
     .join(' ');
 
   return {
@@ -47,7 +47,22 @@ const findById =  async (id) => {
   })
 };
 
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return true;
+};
+
+const create = async (firstName, middleName, lastName) => connection.execute(
+    'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
+    [firstName, middleName, lastName],
+);
+
 module.exports = {
   getAll,
   findById,
+  isValid,
+  create,
 };
