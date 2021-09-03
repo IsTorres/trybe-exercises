dae28402fac7bce495997ed8af96165f92404fd0
isTorres <ismaeltorres612@gmail.com>
2021-09-02 21:10:12 -0300

    cria metodo post

--- back-end/Bloco_26/model-example/index.js
+++ back-end/Bloco_26/model-example/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3003;
 
 const Authors = require('./models/Author');
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
   const author = await Authors.getAll();
@@ -18,5 +20,14 @@ app.get('/authors/:id', async (req, res) => {
   })
   return res.status(200).json(author);
 });
+
+app.post('/authors', async (req, res) => {
+  const { first_name, middle_name, last_name } = req.body;
+
+  if (!Authors.isValid(first_name, middle_name, last_name)) return res.status(400).json({ message: 'Dados inválidos' });
+
+  await Authors.create(first_name, middle_name, last_name);
+  return res.status(201).json({ message: 'Autor criado com sucesso'});
+})
 
 app.listen(port, () => console.log(`app Listen on port ${port}`));
