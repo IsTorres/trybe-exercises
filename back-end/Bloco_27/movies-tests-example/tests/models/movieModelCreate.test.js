4bb2e04e352ca00c31d27f464ae4ee013dadac37
isTorres <ismaeltorres612@gmail.com>
2021-09-09 23:59:11 -0300

    comeco do tdd

--- back-end/Bloco_27/movies-tests-example/tests/models/movieModelCreate.test.js
+++ back-end/Bloco_27/movies-tests-example/tests/models/movieModelCreate.test.js
@@ -3,3 +3,25 @@ const { expect } = require('chai');
 const MoviesModel = {
   create: () => {},
 };
+
+describe('Insere um novo Filme no DB', () => {
+  const payloadMovie = {
+    title: 'title-example',
+    directedBy: 'someone',
+    realeseYear: 2000,
+  };
+
+  describe('Quando é inserido com sucesso', () => {
+    it('retorna um objeto', async () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.be.a('object');
+    });
+
+    it('Tal objeto possui o "id" do novo filme inserido', () => {
+      const response = await MoviesModel.create(payloadMovie);
+
+      expect(response).to.have.a.property('id');
+    });
+  })
+});
