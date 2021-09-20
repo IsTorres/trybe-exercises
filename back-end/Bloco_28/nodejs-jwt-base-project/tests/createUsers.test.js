ee2e190ff17560fc3e5e0446f484e1c50444b5ed
isTorres <ismaeltorres612@gmail.com>
2021-09-20 19:24:02 -0300

    set the 'before' test

--- back-end/Bloco_28/nodejs-jwt-base-project/tests/createUsers.test.js
+++ back-end/Bloco_28/nodejs-jwt-base-project/tests/createUsers.test.js
@@ -1,5 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
+const server = require('../src/api/app');
 
 chai.use(chaiHttp);
 
@@ -8,6 +9,15 @@ const { expect } = chai;
 describe('POST /api/user', () => {
   describe('quando é criado com sucesso', () => {
     let response = {};
+
+    before(async () => {
+      response = await chai.request(server)
+        .post('/api/users')
+        .send({
+          username: 'jane',
+          password: 'senha123'
+        });
+    });
 
     it('retorna status cod 201', () => {
       expect(response).to.have.status(201);
