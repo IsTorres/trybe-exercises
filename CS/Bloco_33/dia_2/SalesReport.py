d63d1cb0b279b5f874d832d443a34fb1a46e1c9a
isTorres <ismaeltorres612@gmail.com>
2021-12-08 16:02:53 -0300

    POO - herança

---
+++ CS/Bloco_33/dia_2/SalesReport.py
@@ -0,0 +1,77 @@
+# class SalesReport(ABC):
+#     def __init__(self, export_file):
+#         self.export_file = export_file + '.json'
+        
+
+#     def build(self):
+#         """ Lógica para a entidade 'se criar',
+#         ou seja, criar um relatório imprimível. """
+#         return [
+#             {
+#             'Coluna 1': 'Dado 1',
+#             'Coluna 2': 'Dado 2',
+#             'Coluna 3': 'Dado 3'
+#             },
+#             {
+#             'Coluna 1': 'Dado A',
+#             'Coluna 2': 'Dado B',
+#             'Coluna 3': 'Dado C'
+#             }
+#         ]
+#     @abstractmethod
+#     def serialize(self):
+#         with open(self.export_file, 'w') as file:
+#             json.dump(self.build(), file)
+
+
+# class SalesReportCSV(SalesReport):
+#     def serialize(self):
+#         with open(self.export_file + '.csv', 'w') as file:
+#             csv.DictWriter(self.build(), file)
+#             # json.dump(self.build(), file)
+
+import json
+import csv
+
+
+class SalesReport():
+    def __init__(self, export_file):
+        self.export_file = export_file + '.json'
+        
+
+    def build(self):
+        """ Lógica para a entidade 'se criar',
+        ou seja, criar um relatório imprimível. """
+        return [
+            {
+            'Coluna 1': 'Dado 1',
+            'Coluna 2': 'Dado 2',
+            'Coluna 3': 'Dado 3'
+            },
+            {
+            'Coluna 1': 'Dado A',
+            'Coluna 2': 'Dado B',
+            'Coluna 3': 'Dado C'
+            }
+        ]
+    
+    def serialize(self):
+        with open(self.export_file, 'w') as file:
+            json.dump(self.build(), file)
+
+
+class SalesReportCSV(SalesReport):
+    def serialize(self):
+        with open(self.export_file + '.csv', 'w') as file:
+            # sempre bom ter um 'header'
+            headres = ['Coluna 1', 'Coluna 2', 'Coluna 3',]
+            writer = csv.DictWriter(file, headres)
+            writer.writeheader()
+            # self.build() é o obj
+            for item in self.build():
+                writer.writerow(item)
+
+
+meu_relatorio_de_vendas = SalesReport('meu_relatorio')
+
+meu_relatorio_de_vendas.serialize()
